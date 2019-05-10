0ba81e1de9c517f4594ee0ee1370b964f7010f74
Uriel Yair <Uriel@URIEL-PC>
2019-05-10 19:00:52 +0300

    1. fix parsers by adding default cases to everyone of them. 2. fix all nullable parsers when they derivating epsilon by callig back_token() method.

--- Parser.c
+++ Parser.c
@@ -1,12 +1,7 @@
 #include "Parser.h"
 #include "Token.h"
 
-// TODO: check for error handling.
-// todo: check returned value in every match call.
-// todo: fix default cases with error printing and handling. in each.
-// TODO: fix nullable variables with back_token call.
-// TODO: add match call to token identify.
-
+//TODO: add messeges printing about errors and rules in use.
 
 void parse_PROGRAM()
 {
@@ -20,11 +15,15 @@ void parse_PROGRAM()
 		match(TOKEN_KW_END);
 		parse_FUNC_DEFINITIONS();
 		break;
+	default:
+		while (t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
-
-
 void parse_VAR_DEFINITIONS()
 {
 	Token* t = next_token();
@@ -35,17 +34,28 @@ void parse_VAR_DEFINITIONS()
 		parse_VAR_DEFINITION();
 		parse_VAR_DEFINITIONS_SUFFIX();
 		break;
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
+	
 }
 void parse_VAR_DEFINITIONS_SUFFIX()
 {
-	// nullable
+	// nullable - done
 	Token* t = next_token();
 	switch (t->kind)
 	{
 	case TOKEN_SEMICOLON:
 	{
-		Token* tokenToCheck = peekN(t, 1);
+		Token* tokenToCheck = peekN(t, 1); // looking to check what is the kind of the next token to decise how to act next.
 		if (tokenToCheck->kind == TOKEN_KW_INTEGER || tokenToCheck->kind == TOKEN_KW_REAL)
 			parse_VAR_DEFINITIONS();
 		else
@@ -55,11 +65,19 @@ void parse_VAR_DEFINITIONS_SUFFIX()
 	case TOKEN_CLOSE_ROUND_BRACKETS: // EPSILON
 		back_token();
 		break;
+
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
-
-
 void parse_VAR_DEFINITION()
 {
 	Token* t = next_token();
@@ -71,8 +89,17 @@ void parse_VAR_DEFINITION()
 		parse_VARIABLES_LIST();
 		break;
 
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
-
 }
 void parse_TYPE()
 {
@@ -82,10 +109,19 @@ void parse_TYPE()
 	case TOKEN_KW_REAL:
 	case TOKEN_KW_INTEGER:
 		break;
-
+	
+	default:
+		while (
+			t->kind != TOKEN_ID
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
 void parse_VARIABLES_LIST()
 {
 	Token* t = next_token();
@@ -96,28 +132,47 @@ void parse_VARIABLES_LIST()
 		parse_VARIABLES_LIST_SUFFIX();
 		break;
 
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
-
 void parse_VARIABLES_LIST_SUFFIX()
 {
-	// nullable
+	// nullable - done
 
 	Token* t = next_token();
 	switch (t->kind)
 	{
-	case TOKEN_SEMICOLON:
+	case TOKEN_COMMA:
 		parse_VARIABLE();
 		parse_VARIABLES_LIST_SUFFIX();
 		break;
 
-
-
+	case TOKEN_SEMICOLON:
+	case TOKEN_CLOSE_ROUND_BRACKETS:
+		back_token();
+		break;
+		
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
-
 void parse_VARIABLE()
 {
 	Token* t = next_token();
@@ -126,12 +181,21 @@ void parse_VARIABLE()
 	case TOKEN_ID:
 		parse_VARIABLE_SUFFIX();
 		break;
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS && t->kind != TOKEN_COMMA
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
 void parse_VARIABLE_SUFFIX()
 {
-	//nullable
+	//nullable - done
 	Token* t = next_token();
 	switch (t->kind)
 	{
@@ -147,6 +211,15 @@ void parse_VARIABLE_SUFFIX()
 		back_token();
 		break;
 
+		while (
+			t->kind != TOKEN_SEMICOLON && t->kind != TOKEN_CLOSE_ROUND_BRACKETS && t->kind != TOKEN_COMMA && t->kind != TOKEN_OP_EQUAL
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 
 	}
 }
@@ -162,14 +235,22 @@ void parse_FUNC_DEFINITIONS()
 		parse_FUNC_DEFINITION();
 		parse_FUNC_DEFINITIONS_SUFFIX();
 		break;
+	default:
+		while (
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 
 
 }
 void parse_FUNC_DEFINITIONS_SUFFIX()
 {
-	// nullable
-	// TODO: keep checking follow from here and add cases.
+	// nullable - done
+	
 	Token* t = next_token();
 	switch (t->kind)
 	{
@@ -180,6 +261,18 @@ void parse_FUNC_DEFINITIONS_SUFFIX()
 		parse_FUNC_DEFINITIONS_SUFFIX();
 		break;
 
+	case TOKEN_END_OF_FILE:
+		back_token();
+		break;
+
+	default:
+		while (
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 
 }
@@ -198,6 +291,17 @@ void parse_FUNC_DEFINITION()
 		match(TOKEN_CLOSE_ROUND_BRACKETS);
 		parse_BLOCK();
 		break;
+
+	default:
+		while (
+			t->kind != TOKEN_KW_VOID && t->kind != TOKEN_KW_REAL && t->kind != TOKEN_KW_INTEGER 
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
 void parse_RETURNED_TYPE()
@@ -211,12 +315,21 @@ void parse_RETURNED_TYPE()
 	case TOKEN_KW_INTEGER:
 		parse_TYPE();
 		break;
+	default:
+		while (
+			t->kind != TOKEN_ID
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
 void parse_PARAM_DEFINITIONS()
 {
-	// nullable
+	// nullable - done
 
 	Token* t = next_token();
 	switch (t->kind)
@@ -225,10 +338,22 @@ void parse_PARAM_DEFINITIONS()
 	case TOKEN_KW_INTEGER:
 		parse_VAR_DEFINITIONS();
 		break;
+	case TOKEN_CLOSE_ROUND_BRACKETS:
+		back_token();
+		break;
 
+	default:
+		while (
+			t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
 void parse_STATEMENTS()
 {
 	Token* t = next_token();
@@ -241,11 +366,21 @@ void parse_STATEMENTS()
 		match(TOKEN_SEMICOLON);
 		parse_STATEMENTS_SUFFIX();
 		break;
+	default:
+		while (
+			t->kind != TOKEN_KW_END && t->kind != TOKEN_CLOSE_CURLY_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
 void parse_STATEMENTS_SUFFIX()
 {
-	// nullable
+	// nullable -done
 
 	Token* t = next_token();
 	switch (t->kind)
@@ -255,32 +390,54 @@ void parse_STATEMENTS_SUFFIX()
 	case TOKEN_OPEN_CURLY_BRACKETS:
 		parse_STATEMENTS();
 		break;
+
+	case TOKEN_KW_END:
+	case TOKEN_CLOSE_CURLY_BRACKETS:
+		back_token();
+		break;
+
+	default:
+		while (
+			t->kind != TOKEN_KW_END && t->kind != TOKEN_CLOSE_CURLY_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
-
 void parse_STATEMENT()
 {
 	Token* t = next_token();
 	switch (t->kind)
 	{
 
-	case TOKEN_KW_RETURN:
-		parse_RETURN_SUFFIX();
-		break;
-
 	case TOKEN_OPEN_CURLY_BRACKETS:
 		parse_BLOCK();
+		break;
+
+	case TOKEN_KW_RETURN:
+		parse_RETURN_SUFFIX();
 		break;
 
 	case TOKEN_ID:
 		parse_STATEMENT_SUFFIX();
 		break;
 
-
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
-
 void parse_STATEMENT_SUFFIX() 
 {
 	Token* t = next_token();
@@ -297,12 +454,23 @@ void parse_STATEMENT_SUFFIX()
 		match(TOKEN_OP_EQUAL);
 		parse_EXPRESSION();
 		break;
+
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 	
 }
 void parse_RETURN_SUFFIX()
 {
-	//nullable
+	//nullable - done
 	Token* t = next_token();
 	switch (t->kind)
 	{
@@ -310,6 +478,20 @@ void parse_RETURN_SUFFIX()
 	case TOKEN_REAL_NUMBER:
 	case TOKEN_ID:
 		parse_EXPRESSION();
+		break;
+	case TOKEN_SEMICOLON:
+		back_token();
+		break;
+
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
 		break;
 	}
 }
@@ -324,6 +506,16 @@ void parse_BLOCK()
 		parse_STATEMENTS();
 		match(TOKEN_CLOSE_CURLY_BRACKETS);
 		break;
+	default:
+		while (
+			t->kind != TOKEN_KW_VOID && t->kind != TOKEN_KW_REAL && t->kind != TOKEN_KW_INTEGER && t->kind != TOKEN_SEMICOLON
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
 void parse_PARAMETERS_LIST()
@@ -335,29 +527,54 @@ void parse_PARAMETERS_LIST()
 	case TOKEN_ID:
 		parse_VARIABLES_LIST();
 		break;
+	case TOKEN_CLOSE_ROUND_BRACKETS:
+		back_token();
+		break;
+	default:
+		while (
+			t->kind != TOKEN_CLOSE_ROUND_BRACKETS
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
+		break;
 	}
 }
 void parse_EXPRESSION()
 {
 	Token* t = next_token();
+	Token* tokenToCheck;
+
 	switch (t->kind)
 	{
 	case TOKEN_INT_NUMBER:
 	case TOKEN_REAL_NUMBER:
 		break;
 	case TOKEN_ID:
-		t = next_token();
-		if (t->kind == TOKEN_OP_EQUAL)
+		
+		tokenToCheck = peekN(t, 1); // looking to check what is the kind of the next token to decise how to act next.
+		
+		if (tokenToCheck->kind == TOKEN_OP_EQUAL)
 		{
-			t = back_token();
 			match(TOKEN_OP_EQUAL);
 			parse_EXPRESSION();
 		}
 		else
 		{
-			t= back_token();
 			parse_VARIABLE();
 		}
+		break;
+	default:
+		while (
+			t->kind != TOKEN_SEMICOLON
+			&&
+			t->kind != TOKEN_END_OF_FILE)
+		{
+			t = next_token();
+		}
+		back_token();
 		break;
 	}
 }
