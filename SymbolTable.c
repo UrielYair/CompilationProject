fbd0096d75a173d2ef3ff0c0baf8e23e13b1a399
Uriel Yair <Uriel@URIEL-PC>
2019-07-01 20:14:02 +0300

    Add SymbolTable and implement some of the functions.

---
+++ SymbolTable.c
@@ -0,0 +1,75 @@
+#include "SymbolTable.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+SymbolTable* currentTable = NULL;
+
+SymbolTable* make_table() {
+		
+	SymbolTable* newTable;
+	newTable->father = currentTable;
+	newTable->currentSymbolTable = ht_new();
+
+	currentTable = newTable;
+	return currentTable;
+}
+
+SymbolTable* pop_table() {
+	ht_del_hash_table(currentTable->currentSymbolTable);
+	currentTable = currentTable->father;
+	return currentTable;
+}
+
+
+ID_Information* insert(char* id_name) {
+	// TODO: check if already in the table.
+	ID_Information* info = (ID_Information*)malloc(sizeof(ID_Information));
+	info->name = strdup(id_name);
+	ht_insert(currentTable->currentSymbolTable, id_name, info);
+	return info;
+}
+
+ID_Information* lookup(char* id_name) {
+	// TODO: maybe error printing is needed.
+	return ht_search(currentTable->currentSymbolTable, id_name);
+}
+
+ID_Information* find(char* id_name) {
+	// TODO: maybe error printing is needed.
+	SymbolTable* tempSymbolTableToCheck = currentTable;
+	while (tempSymbolTableToCheck != NULL)
+	{
+		ID_Information* result = ht_search(currentTable->currentSymbolTable, id_name);
+		if (result != NULL)
+			return result;
+		tempSymbolTableToCheck = tempSymbolTableToCheck->father;
+	}
+	return NULL;
+
+}
+
+
+// TODO: keep editing !!!
+void set_id_type(ID_Information* id_entry, char* id_type) {
+	id_entry->name = strdup(id_type);
+}
+char* get_id_type(ID_Information* id_entry) {
+	if(strcmp(id_entry->functionOrVariable,"variable")==0)
+		return id_entry->ID_Type;
+	else
+		return id_entry->returnedType;
+}
+
+
+
+/*
+functions from hash table for reference.
+
+ht_hash_table*	ht_new();
+void			ht_del_hash_table(ht_hash_table* ht);
+
+void			ht_insert(ht_hash_table* ht, const char* key, const ID_Information* value);
+ID_Information* ht_search(ht_hash_table* ht, const char* key);
+void			ht_delete(ht_hash_table* h, const char* key);
+*/
---
+++ SymbolTable.h
@@ -0,0 +1,42 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include "HashTable.h"
+
+typedef struct SymbolTable {
+
+	SymbolTable* father;
+	ht_hash_table* currentSymbolTable;
+
+} SymbolTable;
+
+typedef struct ID_Information {
+	char* name;
+	char* functionOrVariable;
+	bool wasUsed;
+
+	//ID_Information - variables
+	char* ID_Type;
+	int sizeOfArray; // (in case of array.)
+	// TODO: when allocate memory check that the size of the array is not 0.
+
+
+	//ID_Information - functions declaration
+	char* returnedType;
+	int numOfArguments;
+	ID_Information** listOfArguments;
+
+} ID_Information;
+
+
+//	*******************   functions   *******************
+
+	SymbolTable*	make_table();
+	SymbolTable*	pop_table();
+	ID_Information*	insert(char* id_name);
+	ID_Information* lookup(char* id_name);
+	ID_Information* find(char* id_name);
+	void			set_id_type(ID_Information* id_entry, char* id_type);
+	char*			get_id_type(ID_Information* id_entry);
+
+#endif  // SYMBOL_TABLE_H
