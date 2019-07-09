8aa7fa1f7d45740760507efb90a4e6450c20e133
Uriel Yair <Uriel@URIEL-PC>
2019-07-09 17:50:48 +0300

    Add utility methods to handle Symbol Table.

--- SymbolTable.c
+++ SymbolTable.c
@@ -1,5 +1,7 @@
 #include "SymbolTable.h"
 #include "HashTable.c"
+#include "Token.h"
+
 #include <string.h>
 #include <stdlib.h>
 
@@ -25,13 +27,47 @@ SymbolTable* pop_table() {
 }
 
 
+ID_Information* new_ID_Information(char* name) {
+
+	ID_Information* new_id = (ID_Information*)malloc(sizeof(ID_Information));
+
+	if (new_id != NULL)
+	{
+		new_id->name = strdup(name);
+		new_id->functionOrVariable = NULL;
+		new_id->wasUsed = false;
+		new_id->ID_Type = NULL;
+		new_id->sizeOfArray = -1;
+		new_id->isArray = false;
+		new_id->returnedType = NULL;
+		new_id->numOfArguments = -1;
+		new_id->listOfArguments = NULL;
+	}
+	return new_id;
+}
+
+void delete_ID_Information(ID_Information* idToDelete) {
+
+	free(idToDelete->name);
+
+	if (strcmp(idToDelete->functionOrVariable, "function") == 0)
+	{
+		free(idToDelete->returnedType);
+		// TODO: call delete list.
+		//idToDelete->listOfArguments
+		free(idToDelete->listOfArguments);
+	}
+	free(idToDelete->functionOrVariable);
+	free(idToDelete->ID_Type);
+	
+	free(idToDelete);
+}
+
 ID_Information* insert(char* id_name) {
-	// TODO: check if already in the table.
 	ID_Information* info = lookup(id_name);
 	if (info != NULL) // TODO: add error printing.
-		return NULL;	
-	info = (ID_Information*)malloc(sizeof(ID_Information));
-	info->name = strdup(id_name);
+		return NULL;
+	info = new_ID_Information(id_name);
 	ht_insert(currentTable->currentSymbolTable, id_name, info);
 	return info;
 }
@@ -55,8 +91,43 @@ ID_Information* find(char* id_name) {
 
 }
 
-void set_id_type(ID_Information* id_entry, char* id_type) {
-	id_entry->name = strdup(id_type);
+void set_id_info(ID_Information* id_entry, char* whatToUpdate, void* value) {
+	
+	// name
+	if (strcmp(whatToUpdate, "name") == 0)
+		id_entry->name = strdup((char*)value);
+	
+	// functionOrVariable
+	else if (strcmp(whatToUpdate, "functionOrVariable") == 0)
+		id_entry->functionOrVariable = strdup((char*)value);
+	
+	// wasUsed
+	else if (strcmp(whatToUpdate, "wasUsed") == 0)
+		id_entry->wasUsed = (bool)value;
+	
+	// ID_Type
+	else if (strcmp(whatToUpdate, "ID_Type") == 0)
+		id_entry->ID_Type = strdup((char*)value);
+	
+	// sizeOfArray
+	else if (strcmp(whatToUpdate, "sizeOfArray") == 0)
+		id_entry->sizeOfArray = (int)value;
+	
+	// returnedType
+	else if (strcmp(whatToUpdate, "returnedType") == 0)
+		id_entry->returnedType = strdup((char*)value);
+	
+	// numOfArguments
+	else if (strcmp(whatToUpdate, "numOfArguments") == 0)
+		id_entry->numOfArguments = (int)value;
+	
+	// listOfArguments
+	else if (strcmp(whatToUpdate, "listOfArguments") == 0)
+		id_entry->listOfArguments = (struct IDInfoLinkNode*)value;
+	
+	// isArray
+	else if (strcmp(whatToUpdate, "isArray") == 0)
+		id_entry->listOfArguments = (bool)value;
 }
 char* get_id_type(ID_Information* id_entry) {
 	if(strcmp(id_entry->functionOrVariable,"variable")==0)
@@ -64,9 +135,7 @@ char* get_id_type(ID_Information* id_entry) {
 	else
 		return id_entry->returnedType;
 }
-void free_ID_info(ID_Information* id_entry) {
-	// TODO: Implement!!
-}
+
 
 bool wereAllIDsUsed () 
 { 
@@ -95,4 +164,23 @@ bool wereAllIDsUsed ()
 		printf("%u ID's wasn't in use.\n", counterOfIDsWhichNotUsed);
 
 	return everyIDWasInUse;
+}
+
+bool isIDExistInSymbolTable(char* id_name) {
+	ID_Information* id = find(id_name);
+	if (id == NULL)
+		return false;
+	return true;
+}
+
+void checkIfIDAlreadyDeclared(char* id_name) {
+	if (!isIDExistInSymbolTable(id_name))
+		printf("The ID: %s in line %u must be declared before being used.\n", id_name, getCurrentToken()->lineNumber);
+}
+
+bool isFunction(char* id_name) {
+	ID_Information* idToCheck = find(id_name);
+	if (strcmp(idToCheck->functionOrVariable, "function") == 0)
+		return true;
+	return false;
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -14,21 +14,24 @@ typedef struct SymbolTable {
 
 typedef struct ID_Information {
 
-
 	char* name;
-	char* functionOrVariable;
+	char* functionOrVariable;		// function, variable
 	bool wasUsed;
 
 	//ID_Information - variables
-	char* ID_Type;		// real, integer
-	int sizeOfArray;	// in case of array.
+	char* ID_Type;					// real, integer
+	int sizeOfArray;				// in case of array.
+	bool isArray;
 	
 	//ID_Information - functions declaration
-	char* returnedType;
+	char* returnedType;				// real, integer, void
 	int numOfArguments;
 	struct IDInfoLinkNode* listOfArguments;
 
 } ID_Information;
+
+ID_Information* new_ID_Information(char* name);
+void delete_ID_Information(ID_Information* idToDelete);
 
 
 //	*******************   functions   *******************
@@ -38,9 +41,15 @@ typedef struct ID_Information {
 	ID_Information*	insert(char* id_name);
 	ID_Information* lookup(char* id_name);
 	ID_Information* find(char* id_name);
-	void			set_id_type(ID_Information* id_entry, char* id_type);
+	void			set_id_info(ID_Information* id_entry, char* whatToUpdate, void* value);
 	char*			get_id_type(ID_Information* id_entry);
-	void			free_ID_info(ID_Information* id_entry);
+	
+	ID_Information* new_ID_Information(char* name);
+	void delete_ID_Information(ID_Information* idToDelete);
 	bool			wereAllIDsUsed();
 	
+	bool isIDExistInSymbolTable(char* id_name);
+	void checkIfIDAlreadyDeclared(char* id_name);
+	bool isFunction(char* id_name);
+
 #endif  // SYMBOL_TABLE_H
