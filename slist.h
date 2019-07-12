75924bff1e563ad6979f4c5f2876bce6b35d5545
Uriel Yair <Uriel@URIEL-PC>
2019-07-12 07:33:08 +0300

    Add slistsConcat() declaration to the header file.

--- slist.h
+++ slist.h
@@ -29,5 +29,6 @@ void slist_insert_before(slist* list, snode* node, snode* previous, void* data);
 snode* slist_insert_after(slist* list, snode* node, void* data);
 void slist_for_each(const slist* list, slist_forfn fun);
 unsigned int slist_get_count(const slist* list);
+void* slistsConcat(slist* headList, slist* tailList);
 
 #endif /* SLIST_H */
